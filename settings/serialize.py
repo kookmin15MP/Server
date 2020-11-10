76ed04fd6b1a135d8def4b5fb8e971c3392357c5
yeonsookim40282628 <gjb2628@gmail.com>
2020-11-10 11:42:03 +0900

    Lecture, Basket serializer 추가

--- settings/serialize.py
+++ settings/serialize.py
@@ -1,5 +1,6 @@
 from typequery import GenericMethod
 
+from api.models.lecture import Lecture, Basket
 from api.models.user import User
 
 serialize = GenericMethod('serialize')
@@ -14,10 +15,45 @@ def serialize(value, **kwargs):
     return value
 
 
+@serialize.of(list)
+def serialize(input_list, **kwargs):
+    class_name = input_list[0].__class__.__name__
+    result_list = list()
+    for i in input_list:
+        result_list.append(serialize(i))
+
+    result = {
+        class_name: result_list
+    }
+    return result
+
+
 @serialize.of(User)
 def serialize(user, **kwargs):
     result = {
         'id': user.id,
         'email': user.email,
+    }
+    return result
+
+
+@serialize.of(Lecture)
+def serialize(lecture, **kwargs):
+    result = {
+        'lecture_id': lecture.id,
+        'lecture_name': lecture.lecture_name,
+        'professor_name': lecture.professor_name,
+        'lecture_time': lecture.lecture_time,
+        'limited_people': lecture.limited_people,
+    }
+    return result
+
+
+@serialize.of(Basket)
+def serialize(basket, **kwargs):
+    result = {
+        'basket_id': basket.id,
+        'user_id': basket.user_id,
+        'lecture_id': basket.lecture_id,
     }
     return result
